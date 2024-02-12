b396c30bc6c1f3dd58e035c8ab860d4900414209
Ramanujam Srinivasan <ramanujam.rs@gmail.com>
2024-02-12 11:09:03 -0600

    Initial commit

---
+++ src/main/java/com/ramanujamrs/graphqlstarter/GraphqlStarterApplication.java
@@ -0,0 +1,13 @@
+package com.ramanujamrs.graphqlstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlStarterApplication.class, args);
+	}
+
+}
